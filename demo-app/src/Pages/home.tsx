dee75d2785304f6f37d7393592dfc67e77b011f7
SujanSanjula96 <sanjulasujan@gmail.com>
2022-10-27 22:41:12 +0530

    Add POST api

--- demo-app/src/Pages/home.tsx
+++ demo-app/src/Pages/home.tsx
@@ -5,12 +5,24 @@ import  { useAuthContext } from '@asgardeo/auth-react';
 import { UserProvider } from '../providers/UserProvider';
 import jwt from 'jwt-decode'
 import ReactJson from "react-json-view";
-import { Avatar, Box, Button, Typography } from "@mui/material";
+import { Avatar, Box, Button, Modal, TextField, Typography } from "@mui/material";
 import JSONModal from "../components/JSONModal";
 import { DataGrid, gridClasses } from '@mui/x-data-grid';
 import { grey } from '@mui/material/colors';
 
 import Actions from '../components/actions';
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 interface IssueInterface {
   name?: string
@@ -27,6 +39,8 @@ const HomePage = () => {
   const [ isOrganizationLoaded, setIsOrganizationLoaded] = useState<boolean>(false);
   const [ responseList, setResponseList ] = useState<IssueInterface[]>([]);
   const [ isLoaded, setIsLoaded ] = useState<boolean>(false);
+  const [ openNewIssue, setOpenNewIssue ] = useState<boolean>(false);
+  const [ newIssue,  setNewIssue ] = useState<string>("");
 
   const appList = [{name: "Issue 1"},{name: "Issue 2"},{name: "Issue 3"}];
 
@@ -85,6 +99,23 @@ const HomePage = () => {
     []
   );
 
+  const handleClose = () => {
+    setOpenNewIssue(false);
+  }
+
+  const handleChange = e => {
+    setNewIssue(e.target.value);
+  }
+
+  const handleCreate = async () => {
+    const response = await httpRequest({
+      data: newIssue,
+      method: "POST",
+      url: "https://7f092d26-d233-4e7d-b0da-1a23893c68da-prod.e1-us-east-azure.preview-dv.choreoapis.dev/urow/echo-service/1.0.0/names",
+    });
+    setOpenNewIssue(false);
+  }
+
   return ( 
     {isOrganizationLoaded} && {isLoaded} &&
     <Box sx={{mt:2}}>
@@ -94,7 +125,7 @@ const HomePage = () => {
             alignItems="center"
             sx={{width:"100%"}}>
             <Box m={2} sx={{width:"100%",  height:500} }>
-                <Button variant='contained' sx={{mt:3}} onClick={() => {console.log("")}} >
+                <Button variant='contained' sx={{mt:3}} onClick={() => {setOpenNewIssue(true)}} >
                     New Issue
                 </Button>
                 <Box m={2}>
@@ -123,6 +154,36 @@ const HomePage = () => {
             </Box>
         </Box>
 
+        <Modal
+          open={openNewIssue}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style} >
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Create New Issue
+            </Typography>
+            <TextField
+            required
+            id="outlined-required"
+            value={newIssue}
+            label="Issue"
+            onChange={handleChange}
+            sx={{
+              width: 300, mt:2, mb:3
+            }}
+            />
+            <Box>
+              <Button variant="contained" sx={{mr:2}} onClick={handleCreate}>
+                Create
+              </Button>
+              <Button onClick={handleClose}>
+                Cancel
+              </Button> 
+            </Box>
+          </Box>
+        </Modal>
 
         <Box sx={{backgroundColor:'#eeeeee'}}>
 
