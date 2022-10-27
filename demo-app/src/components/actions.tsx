15dda812af432aae11cba5b6e6bfeb133d4133c6
SujanSanjula96 <sanjulasujan@gmail.com>
2022-10-27 22:51:45 +0530

    Add Delete ApI

--- demo-app/src/components/actions.tsx
+++ demo-app/src/components/actions.tsx
@@ -2,16 +2,26 @@ import { Box, Button, IconButton, Tooltip } from '@mui/material';
 import { Delete, Edit, Preview } from '@mui/icons-material';
 import SettingsIcon from '@mui/icons-material/Settings';
 import { useHistory, useLocation } from 'react-router-dom';
+import { useAuthContext } from '@asgardeo/auth-react';
 
 const Actions = ({ params }) => {
 
   const history = useHistory();
   const location = useLocation();
+  const { state, signOut, getBasicUserInfo, getIDToken, getDecodedIDToken, getAccessToken, httpRequest } = useAuthContext();
+
+  const handleDelete = async () => {
+    const response = await httpRequest({
+      data: params.row.name,
+      method: "DELETE",
+      url: "https://7f092d26-d233-4e7d-b0da-1a23893c68da-prod.e1-us-east-azure.preview-dv.choreoapis.dev/urow/echo-service/1.0.0/names",
+    });
+  }
 
   return (
     <Box>
       <Tooltip title="Manage application roles">
-        <Button variant='outlined'>
+        <Button variant='outlined' onClick={handleDelete}>
           Close
         </Button>
       </Tooltip>
